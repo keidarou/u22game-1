54987128aa92c9c02d2d1bc301f7c0ba56259853
keidarou <kei.spider.asano@gmail.com>
2017-08-04 17:04:24 +0900

    ike

---
+++ Assets/scripts/MainGame/automaticgenerator.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class automaticgenerator : MonoBehaviour
+{
+
+    //------------------変数-------------------------//
+    int[,] map = new int[50, 50];//x座標、y座標
+    bool[,] kakutei = new bool[50, 50];
+    public int nowx, nowy;
+    public int ippen;//一辺の長さ 
+    public int magarukaisuu;
+    public int previous = 0;//0は下向き、1は右向き、２は下向き、３は左向き
+                            //-------------------------------------------------//
+                            // Use this for initialization
+
+    bool check(int x, int y, int range, int movex, int movey)
+    {
+        for (int i = 0; i < range; i++)
+        {
+            if (map[x, y] == 1 && kakutei[x, y] == true)
+            {
+                return false;
+            }
+            x += movex; y += movey;
+        }
+        if (map[x, y] == 0 && kakutei[x, y] == true) { return false; }
+        map[x, y] = 1; kakutei[x, y] = true;
+
+        for (int i = 0; i < range; i++)
+        {
+            x -= movex; y -= movey;
+            map[x, y] = 0; kakutei[x, y] = true;
+        }
+        return true;
+    }
+
+    void Start()
+    {
+        kakutei[ippen, ippen - 2] = true; kakutei[ippen - 1, ippen - 2] = true; kakutei[ippen - 2, ippen - 2] = true; kakutei[ippen, ippen + 2] = true; kakutei[ippen + 1, ippen + 2] = true; kakutei[ippen + 2, ippen + 2] = true;
+        map[ippen, ippen - 2] = 1; map[ippen - 1, ippen - 2] = 1; map[ippen - 2, ippen - 2] = 1; map[ippen, ippen + 2] = 1; map[ippen + 1, ippen + 2] = 1; map[ippen + 2, ippen + 2] = 1;
+        for (int i = 0; i < ippen * 2 + 1; i++)
+        {
+            map[0, i] = 1; map[i, 0] = 1; map[ippen * 2, i] = 1; map[i, ippen * 2] = 1;
+            kakutei[0, i] = true; kakutei[i, 0] = true; kakutei[ippen * 2, i] = true; kakutei[i, ippen * 2] = true;
+        }
+
+        for (int nannkaime = 0; nannkaime < magarukaisuu; nannkaime++)
+        {
+            int muki, range = 0, hantairange = 0;
+            //重力通りに動く玉から
+            while (true)
+            {
+                //0は下向き、1は右向き、２は上向き、３は左向き
+                muki = Random.Range(0, 4);
+                if (muki == previous) { continue; }
+                if (muki == (previous + 2) % 4) { continue; }
+
+                if (muki == 0)
+                {
+                    range = Random.Range(0, nowy);//壁を作る一歩手前で止まる
+                    if (range % 2 == 1) { continue; }
+                    if (!check(nowx, nowy, range, 0, 1)) { continue; }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        map[nowx - 1 + i, nowy - range] = 1;//
+                        kakutei[nowx - 1 + i, nowy - range] = true;//
+                    }
+                    //----------------------------------------------//
+                    if (kakutei[nowx, nowy + 1] == false)
+                    {
+                        while (true)
+                        {
+                            hantairange = Random.Range(2, 16 - nowy);
+                            if (hantairange % 2 == 1) { break; }
+                        }
+                        if (kakutei[nowx, nowy + hantairange] == false)
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                map[nowx - 1 + i, nowy + hantairange] = 1;//
+                            }
+                        }
+                    }
+                    nowy -= range;
+                }
+                if (muki == 1)
+                {
+                    range = Random.Range(0, 15 - nowx);
+                    if (range % 2 == 1) { continue; }
+                    if (!check(nowx, nowy, range, -1, 0)) { continue; }
+
+                    for (int i = 0; i < 3; i++)
+                    {
+                        map[nowx +range, nowy - 1+i] = 1;//
+                        kakutei[nowx + range, nowy - 1 + i] = true;////
+                    }
+                    //----------------------------------------------//
+                    if (kakutei[nowx, nowy + 1] == false)
+                    {
+                        while (true)
+                        {
+                            hantairange = Random.Range(2, 16 - nowy);
+                            if (hantairange % 2 == 1) { break; }
+                        }
+                        if (kakutei[nowx, nowy + hantairange] == false)
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                map[nowx - 1 + i, nowy + hantairange] = 1;//
+                            }
+                        }
+                    }
+                    nowx += range;
+                }
+                if (muki == 2)
+                {
+                    range = Random.Range(0, 15 - nowy);
+                    if (range % 2 == 1) { continue; }
+                    if (!check(nowx, nowy, range, 0, -1)) { continue; }
+                    nowy += range;
+                }
+                if (muki == 3)
+                {
+                    range = Random.Range(0, nowx);
+                    if (range % 2 == 1) { continue; }
+                    if (!check(nowx, nowy, range, 1, 0)) { continue; }
+                    nowx -= range;
+                }
+
+
+                break;
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/scripts/MainGame/movetheball.cs
+++ Assets/scripts/MainGame/movetheball.cs
@@ -13,69 +13,79 @@ public class movetheball : MonoBehaviour
     public GameObject mapgenerator, balldown, ballup;//それぞれのゲームオブジェクト、分からなければ連絡よろ
     int[,] map = new int[50, 50];//マップ
     int down, up;//downは重力に従って落ちるボールが何マス落ちるか、upはその逆
-    public int nowx, nowy;//グリッド表示での現在の座標
+    public int nowupx, nowupy,nowdownx,nowdowny;//グリッド表示での現在の座標
     public float haba;//移動する量、つまり一マスの間隔
+    int karix, kariy;
+    Vector3 upvectormokuteki, upvectornow, downvectornow, downvectormokuteki=Vector3.zero;//移動にはvectorにする必要がある
     //-------------------------------------------------
     //内容としては、玉が動き終わって、スマホの方向、位置が与えられたとき、どの方向に何マス動くかというのを返します。
     //返すといっても変数の中に格納しておくだけです。返り値はないです。
 
-    int Selectrange(int x, int y, int genzaix, int genzaiy)//このx,yはx方向にどれだけ、y方向も同様なので、通常x,y=1,0・0,1・-1,0・0,-1
+    int Selectrange(int x, int y,int nowx,int nowy)//このx,yはx方向にどれだけ、y方向も同様なので、通常x,y=1,0・0,1・-1,0・0,-1
     {
         int count = 0;
         while (true)
         {
-            if (map[genzaix + x, genzaiy + y] == 1)
+            if (map[nowx + x, nowy + y] == 1)
             {
+                karix = nowx;kariy = nowy;
                 return count;//壁に当たったら、何マス行けたかを返す
+
             }
-            genzaix += x; genzaiy += y; count++;
+            nowx += x; nowy += y; count++;
         }
     }
 
-    void selectDirectionandrange(int Direction, int x, int y)//このx,yはgenzaix,genzaiyの意味です。すみません
+    void selectDirectionandrange(int Direction)//このx,yはgenzaix,genzaiyの意味です。すみません
     {
         //方向を把握していないので、適当に0を上、1を右、2を下、3を左にします
         if (Direction == 2)//上向きの重力
         {
-            up = Selectrange(0, -1, x, y);
-            down = Selectrange(0, 1, x, y);
+            up = Selectrange(0, -1,nowupx,nowupy);
+            nowupx = karix;nowupy = kariy;
+            down = Selectrange(0, 1,nowdownx,nowdowny);
+            nowdownx = karix;nowdowny = kariy;
         }
         else if (Direction == 1)//右
         {
-            up = Selectrange(-1, 0, x, y);
-            down = Selectrange(1, 0, x, y);
+            up = Selectrange(-1, 0, nowupx, nowupy);
+            nowupx = karix; nowupy = kariy;
+            down = Selectrange(1, 0, nowdownx, nowdowny);
+            nowdownx = karix; nowdowny = kariy;
+            
         }
         else if (Direction == 0)//下
         {
-            up = Selectrange(0, 1, x, y);
-            down = Selectrange(0, -1, x, y);
+            up = Selectrange(0, 1, nowupx, nowupy);
+            nowupx = karix; nowupy = kariy;
+            down = Selectrange(0, -1, nowdownx, nowdowny);
+            nowdownx = karix; nowdowny = kariy;
         }
         else//左
         {
-            up = Selectrange(1, 0, x, y);
-            down = Selectrange(-1, 0, x, y);
+            up = Selectrange(1, 0, nowupx, nowupy);
+            nowupx = karix; nowupy = kariy;
+            down = Selectrange(-1, 0, nowdownx, nowdowny);
+            nowdownx = karix; nowdowny = kariy;
         }
         //これで代入おーわり。あとはこれに従っていどうして
     }
 
     bool ballmove()//玉を動かすぞい
     {
-        balldown.transform.position = balldown.transform.position;//ダウンボールの位置を代入
-        ballup.transform.position = ballup.transform.position;//アップボールの位置を代入
+        //balldown.transform.position = balldown.transform.position;//ダウンボールの位置を代入
+        //ballup.transform.position = ballup.transform.position;//アップボールの位置を代入
         int movexhoukou = 0, moveyhoukou = 0;//下向きがどの方向に行くか、つまり、x=1,y=0で右方向に１進むみたいな
         if (nowrotation == 0) { movexhoukou = 0; moveyhoukou = -1; }//もし、下向きなら
         if (nowrotation == 1) { movexhoukou = 1; moveyhoukou = 0; }//右
         if (nowrotation == 2) { movexhoukou = 0; moveyhoukou = 1; }//上
         if (nowrotation == 3) { movexhoukou = -1; moveyhoukou = 0; }//左
-        Vector3 upvectormokuteki, upvectornow, downvectornow, downvectormokuteki;//移動にはvectorにする必要がある
         downvectormokuteki = balldown.transform.position;//とりあえず初期化
-        downvectormokuteki += new Vector3(movexhoukou * haba * down, moveyhoukou * haba * down, 0.00f);//目的なので、それに方向×距離を足す
+        downvectormokuteki += new Vector3(movexhoukou * haba * down, moveyhoukou * haba * down, -1.00f);//目的なので、それに方向×距離を足す
         upvectormokuteki = ballup.transform.position;//同様
-        upvectormokuteki += new Vector3(-1 * movexhoukou * haba * up, -1 * moveyhoukou * haba * up, 0.00f);//同様
+        upvectormokuteki -= new Vector3( movexhoukou * haba * up,  moveyhoukou * haba * up, 0.00f);//同様
         upvectornow = ballup.transform.position;//今
-        downvectornow =balldown.transform.position;//同じ
-        ballup.transform.position = Vector3.Lerp(upvectornow, upvectormokuteki, 2);//Lerpですすむ、AnimationCurveであとで速さとか調節、第三引数ようわからないのでデバッグ
-        balldown.transform.position = Vector3.Lerp(downvectornow, downvectormokuteki, 2);
+        downvectornow = balldown.transform.position;//同じ
 
         return true;
     }
@@ -92,11 +102,17 @@ public class movetheball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        upvectornow = ballup.transform.position;
+        downvectornow = balldown.transform.position;
+        Debug.Log(upvectornow.x);
+        Debug.Log(upvectornow.y);
+        ballup.transform.position = Vector3.Slerp(upvectornow, upvectormokuteki, Time.deltaTime);//Lerpですすむ、AnimationCurveであとで速さとか調節、第三引数ようわからないのでデバッグ
+        balldown.transform.position = Vector3.Slerp(downvectornow, downvectormokuteki, Time.deltaTime);
         if (idouchuu == true && nowrotation != acc.getDirection())//もし移動中じゃないかつスマホの向きが変わっていたら（回転されたら
         {
             idouchuu = false;//移動中
             nowrotation = acc.getDirection();//スマホの角度代入
-            selectDirectionandrange(nowrotation, nowx, nowy);//上向きに何マス、下向きに何マス移動するかをメモ
+            selectDirectionandrange(nowrotation);//上向きに何マス、下向きに何マス移動するかをメモ
             ballmove();//動かす！
             idouchuu = true;//もう動いていないよお
         }
